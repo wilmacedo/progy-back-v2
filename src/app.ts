d3eda56830e4f03656dd4e2b10ef63f0c11d5469
Wil Macedo <wil.macedo.sa@gmail.com>
2023-12-16 15:54:29 -0300

    feat: :sparkles: add prisma

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
+import { prisma } from './lib/prisma';
 import { errorHandler } from './utils/error-handler';
 
 export const app = express();
@@ -10,5 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(express.json({ limit: '5mb' }));
 app.use(morgan('dev'));
+
+app.use('/users', async (req, res) => {
+  const users = await prisma.users.findMany();
+
+  res.status(200).json({ users });
+});
 
 app.use(errorHandler);
