28d2f4503a19229878704cd545654e01ec7fd84c
Wil Macedo <wil.macedo.sa@gmail.com>
2023-12-16 17:24:57 -0300

    chore: :zap: update prisma schema

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
-import { prisma } from './lib/prisma';
 import { errorHandler } from './utils/error-handler';
 
 export const app = express();
@@ -11,11 +10,5 @@ app.use(express.json());
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(express.json({ limit: '5mb' }));
 app.use(morgan('dev'));
-
-app.use('/users', async (_, res) => {
-  const users = await prisma.user.findMany();
-
-  res.status(200).json({ users });
-});
 
 app.use(errorHandler);
